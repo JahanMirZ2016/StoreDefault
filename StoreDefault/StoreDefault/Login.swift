c83760e0bfcdd91a9415345ea841003a27c5513a
Unknown <arashqu1ckz@yahoo.com>
2016-12-21 08:44:19 +0330

    Arash: Login section added.

---
+++ StoreDefault/StoreDefault/Login.swift
@@ -0,0 +1,36 @@
+//
+//  Login.swift
+//  StoreDefault
+//
+//  Created by arash on 12/21/16.
+//  Copyright © 2016 arash. All rights reserved.
+//
+
+import UIKit
+
+class Login: UIView {
+
+    @IBOutlet var view : UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        createNib()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        createNib()
+    }
+    
+    
+    func createNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "Register", bundle: bundle)
+        view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        view.frame = bounds
+        view.autoresizingMask = [UIViewAutoresizing.FlexibleWidth , UIViewAutoresizing.FlexibleHeight]
+        addSubview(view)
+    }
+
+
+}
---
+++ StoreDefault/StoreDefault/RegisterVC.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterVC.swift
+//  StoreDefault
+//
+//  Created by arash on 12/20/16.
+//  Copyright © 2016 arash. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
